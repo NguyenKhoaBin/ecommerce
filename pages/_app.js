18679bbb8552f5a48eec3a8170df5b2d857e1eb8
NguyenBin <19516131.bin@student.iuh.edu.vn>
2022-10-13 11:30:06 +0700

    okk

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
-
+import React from "react";
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
